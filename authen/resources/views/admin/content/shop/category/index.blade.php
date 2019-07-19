7131d25dcefb8334d997c34b88e77ccd797e5954
luanc <luanckpt19>
2019-07-19 15:31:02 +0700

    vẫn vậy

---
+++ authen/resources/views/admin/content/shop/category/index.blade.php
@@ -0,0 +1,7 @@
+@extends('admin.layouts.glace')
+@section('title')
+    thànhluânShop
+@endsection
+@section('content')
+    <h1> Quản trị danh mục </h1>
+@endsection
---
+++ authen/resources/views/admin/content/shop/product/index.blade.php
@@ -0,0 +1,7 @@
+@extends('admin.layouts.glace')
+@section('title')
+    thànhluânShop
+@endsection
+@section('content')
+    <h1> Quản trị sản phẩm </h1>
+@endsection
